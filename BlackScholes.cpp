6ffb1e2aeb757e7367356177d6f0e4e75cf6b512
skylee01244 <skylee01244@gmail.com>
2025-12-27 05:42:38 +1100

    Removed prob_ITM variable and intrinsic calculation

--- BlackScholes.cpp
+++ BlackScholes.cpp
@@ -1,4 +1,5 @@
 #include "Headers/BlackScholes.h"
+#include "Headers/Global.h"
 #include <cmath>
 #include <algorithm>
 #include <numbers>
@@ -17,21 +18,9 @@ std::optional<Greeks> BlackScholes::calculate(const Option& option, double S, do
     double T = option.getTimeToExpiry();
     OptionType type = option.getType();
 
-    if (T <= 0) {
-        double intrinsic = 0.0;
-        if (type == OptionType::Call)
-            intrinsic = std::max(0.0, S - K);
-        else
-            intrinsic = std::max(0.0, K - S);
-
-        // Return zero for all Greeks at expiration
+    if (T <= 0 || S <= 0 || K <= 0 || sigma < 0) {
         return std::nullopt;
     }
-    if (S <= 0 || K <= 0 || sigma < 0) {
-        // NaN
-        return std::nullopt;
-    }
-
 
     double d1 = (std::log(S / K) + (r + 0.5 * sigma * sigma) * T) / (sigma * std::sqrt(T));
     double d2 = d1 - sigma * std::sqrt(T);
@@ -52,18 +41,16 @@ std::optional<Greeks> BlackScholes::calculate(const Option& option, double S, do
     g.vega = S * pdf_d1 * sqrtT * 0.01; // change per 1% vol
 
     if (type == OptionType::Call) {
-        g.prob_ITM = cdf_d2; // N(d2) probability of finishing ITM
         g.premium = S * cdf_d1 - K * exp_rT * cdf_d2;
         g.delta = cdf_d1;
         g.rho = K * T * exp_rT * cdf_d2 * 0.01; // change per 1% vol
-        g.theta = (double)(-(S * normalPDF(d1) * sigma) / (2 * sqrtT) - r * K * exp_rT * cdf_d2) / DAYS_IN_YEAR;   // daily theta
+        g.theta = (double)(-(S * normalPDF(d1) * sigma) / (2 * sqrtT) - r * K * exp_rT * cdf_d2) / gbl::TRADING_DAYS;   // daily theta
 
     } else { // Put
-        g.prob_ITM = cdf_neg_d2; // N(-d2) probability of finishing ITM
         g.premium = K * exp_rT * cdf_neg_d2 - S * cdf_neg_d1;
         g.delta = cdf_d1 - 1.0;
         g.rho = -K * T * exp_rT * cdf_neg_d2 * 0.01; // change per 1% vol
-        g.theta = (double)(-(S * normalPDF(d1) * sigma) / (2 * sqrtT) + r * K * exp_rT * cdf_neg_d2) / DAYS_IN_YEAR; // DAYS_IN_YEAR;
+        g.theta = (double)(-(S * normalPDF(d1) * sigma) / (2 * sqrtT) + r * K * exp_rT * cdf_neg_d2) / gbl::TRADING_DAYS;
     }
 
     return g;
