dd7164feebacfe65d91c982b1221011a2c8fe3a0
skylee01244 <skylee01244@gmail.com>
2025-12-28 06:54:37 +1100

    Call SimulateStrategy to implement the Monte Carlo engine

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <optional>
 #include "Headers/Option.h"
 #include "Headers/BlackScholes.h"
+#include "Headers/OptionWizard.h"
 #include "Tests/ParityTest.h"
 
 int main() {
@@ -39,6 +40,20 @@ int main() {
     if (callGreeks && putGreeks) {
         checkPutCallParity(*callGreeks, *putGreeks, callOption, S, r);
     }
+
+    //    Hardcoded for now
+    //    double S = 100.0;   // Initial stock price
+    //    double r = 0.05;    // Risk-free Rate
+    //    double sigma = 0.2; // Volatility
+
+    Option call(110.0, 1.0, OptionType::Call);
+    auto temp = OptionWizard::simulateStrategy(call, 1, std::nullopt, 0, S, 110, 14, r, sigma);
+
+    std::cout << "name: " << temp.strategyName << std::endl
+                << "entryCost: " << temp.entryCost << std::endl
+                << "projectedValue: " << temp.projectedValue << std::endl
+                << "percentProfit: " << temp.profitPercent << std::endl
+                << "probability of Profit: " << temp.pop << std::endl;
 
     return 0;
 }
