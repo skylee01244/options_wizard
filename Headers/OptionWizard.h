d27d9520856818511f102077f6fbbb826c5c20bb
skylee01244 <skylee01244@gmail.com>
2025-12-28 20:47:58 +1100

    Added private function getEstimatedPrice

--- Headers/OptionWizard.h
+++ Headers/OptionWizard.h
@@ -14,6 +14,7 @@ struct result {
 
 class OptionWizard {
 private:
+    double getEstimatedPrice(const Option& i_option, double futureSpot, double futureTimeRemaining, double r, double sigma);
 
 public:
     static result simulateStrategy(const Option& leg1, int quantity1, std::optional<Option> leg2, int quantity2, double currentPrice, double targetPrice, double daysToTarget, double r, double sigma);
