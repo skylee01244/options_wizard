f39a94fecca3af07051b8f83112a25da1161e093
skylee01244 <skylee01244@gmail.com>
2025-12-29 06:08:28 +1100

    Syntax error,,,, forgot ;

--- Headers/BlackScholes.h
+++ Headers/BlackScholes.h
@@ -10,6 +10,6 @@ private:
 
 public:
     [[nodiscard]] static std::optional<Greeks> calculate(double K, double T, OptionType type, double spotPrice, double riskFreeRate, double volatility);
-    [[nodiscard]] static std::optional<double> calculatePremium(double K, double T, OptionType type, double S, double r, double sigma) {
+    [[nodiscard]] static std::optional<double> calculatePremium(double K, double T, OptionType type, double S, double r, double sigma);
     [[nodiscard]] static std::optional<double> calculateIV( const Option& option, double spotPrice, double marketPrice, double riskFreeRate);
 };
