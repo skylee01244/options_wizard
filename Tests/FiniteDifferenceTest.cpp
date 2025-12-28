93994e4f66eaf81e93a9ea62d99e52d39f97e740
skylee01244 <skylee01244@gmail.com>
2025-12-29 05:48:08 +1100

    Added Finite Difference Test

---
+++ Tests/FiniteDifferenceTest.cpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include "../Headers/BlackScholes.h"
+#include "../Headers/Option.h"
+
+inline void runFiniteDifferenceTest() {
+
+    double S = 100.0;
+    double K = 100.0;
+    double T = 1.0;
+    double r = 0.05;
+    double sigma = 0.2;
+    double epsilon = 0.01;
+
+    std::optional<Greeks> result = BlackScholes::calculate(K, T, OptionType::Call, S, r, sigma);
+    if (!result) {
+        std::cerr << "[FAIL] BlackScholes returned nullopt" << std::endl;
+        return;
+    }
+
+    std::optional<Greeks> up   = BlackScholes::calculate(K, T, OptionType::Call, S + epsilon, r, sigma);
+    std::optional<Greeks> down = BlackScholes::calculate(K, T, OptionType::Call, S - epsilon, r, sigma);
+    // (Price(S+h) - Price(S-h)) / 2h
+    double numericalDelta = (up->premium - down->premium) / (2.0 * epsilon);
+    double error = std::abs(result->delta - numericalDelta);
+
+    if (error < 1e-4) {
+        std::cout << "[PASS] Delta is correct." << "\n";
+    } else {
+        std::cout << "[FAIL] Delta is incorrect!" << "\n";
+    }
+}
