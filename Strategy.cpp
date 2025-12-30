ac4a842e320474e27d62ade8156723ee85f6455a
skylee01244 <skylee01244@gmail.com>
2025-12-31 06:32:09 +1100

    Added bearPutSpread n ironCondor

--- Strategy.cpp
+++ Strategy.cpp
@@ -31,12 +31,12 @@ Strategy Strategy::longPut(double K, double T) {
     return s;
 }
 
-Strategy Strategy::bullCallSpread(double K_lower, double K_higher, double T) {
-    if (K_lower >= K_higher) throw std::invalid_argument("ERROR: bullCallSpread");
+Strategy Strategy::bullCallSpread(double K_low, double K_high, double T) {
+    if (K_low >= K_high) throw std::invalid_argument("ERROR: bullCallSpread");
 
     Strategy s("Bull Call Spread");
-    s.addLeg(Option(K_lower, T, OptionType::Call), 1);
-    s.addLeg(Option(K_higher, T, OptionType::Call), -1);
+    s.addLeg(Option(K_low, T, OptionType::Call), 1);
+    s.addLeg(Option(K_high, T, OptionType::Call), -1);
     return s;
 }
 
@@ -47,11 +47,37 @@ Strategy Strategy::straddle(double K, double T) {
     return s;
 }
 
-Strategy Strategy::strangle(double K_lower, double K_higher, double T) {
-    if (K_lower >= K_higher) throw std::invalid_argument("ERROR: strangle");
+Strategy Strategy::strangle(double K_low, double K_high, double T) {
+    if (K_low >= K_high) throw std::invalid_argument("ERROR: strangle");
 
     Strategy s("Long Strangle");
-    s.addLeg(Option(K_lower, T, OptionType::Put), 1);
-    s.addLeg(Option(K_higher, T, OptionType::Call), 1);
+    s.addLeg(Option(K_low, T, OptionType::Put), 1);
+    s.addLeg(Option(K_high, T, OptionType::Call), 1);
+    return s;
+}
+
+Strategy Strategy::bearPutSpread(double K_high, double K_low, double T) {
+    if (K_low >= K_high) throw std::invalid_argument("ERROR: bearPutSpread");
+
+    Strategy s("Bear Put Spread");
+    s.addLeg(Option(K_high, T, OptionType::Put), 1);
+    s.addLeg(Option(K_low, T, OptionType::Put), -1);
+    return s;
+}
+
+Strategy Strategy::ironCondor(double K_put_long, double K_put_short, double K_call_short, double K_call_long, double T) {
+    if (K_put_long >= K_put_short || K_put_short >= K_call_short || K_call_short >= K_call_long) {
+        throw std::invalid_argument("ERROR: ironCondor");
+    }
+
+    Strategy s("Iron Condor");
+    // Bull Put Spread Leg
+    s.addLeg(Option(K_put_short, T, OptionType::Put), -1);
+    s.addLeg(Option(K_put_long, T, OptionType::Put), 1);
+
+    // Bear Call Spread Leg
+    s.addLeg(Option(K_call_short, T, OptionType::Call), -1);
+    s.addLeg(Option(K_call_long, T, OptionType::Call), 1);
+
     return s;
 }
