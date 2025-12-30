97cb71cb5b4eb411da7bd67b7f97a0e0d50d5154
skylee01244 <skylee01244@gmail.com>
2025-12-31 05:49:46 +1100

    Added strategy implementation as well as clean printing and input

--- main.cpp
+++ main.cpp
@@ -1,59 +1,84 @@
 #include <iostream>
 #include <optional>
+#include <vector>
+#include <limits>
+#include <cstring>
+#include <memory>
+#include "Headers/Global.h"
 #include "Headers/Option.h"
 #include "Headers/BlackScholes.h"
 #include "Headers/OptionWizard.h"
+#include "Headers/Strategy.h"
+#include "Headers/VolatilitySurface.h"
 #include "Tests/ParityTest.h"
+#include "Tests/FiniteDifferenceTest.h"
+#include "Tests/MonteCarloConvergenceTest.h"
+#include "UserInterface.h"
 
-int main() {
 
-    double S = 100.0;   // Initial stock price
-    double r = 0.05;    // Risk-free Rate
-    double sigma = 0.2; // Volatility
+int main(int argc, char* argv[]) {
 
-    // Contract Inputs
-    Option callOption(100.0, 1.0, OptionType::Call);
-    Option putOption(100.0, 1.0, OptionType::Put);
-
-    // Calculation
-    std::optional<Greeks> callGreeks = BlackScholes::calculate(callOption, S, r, sigma);
-    std::optional<Greeks> putGreeks = BlackScholes::calculate(putOption, S, r, sigma);
-
-    if(callGreeks) {
-        std::cout << "European Call Option Price: " << callGreeks->premium
-                  << ", Delta: " << callGreeks->delta
-                  << ", Gamma: " << callGreeks->gamma
-                  << ", Vega: " << callGreeks->vega
-                  << ", Theta: " << callGreeks->theta
-                  << ", Rho: " << callGreeks->rho << std::endl;
+    // Tests
+    if (argc > 1 && std::strcmp(argv[1], "--test") == 0) {
+        // Finite Difference Test
+        runFiniteDifferenceTest();
+        runParityTest();
+        runMonteCarloConvergenceTest();
+        return 0;
     }
 
-    if(putGreeks) {
-        std::cout << "European Put Option Price: " << putGreeks->premium
-                  << ", Delta: " << putGreeks->delta
-                  << ", Gamma: " << putGreeks->gamma
-                  << ", Vega: " << putGreeks->vega
-                  << ", Theta: " << putGreeks->theta
-                  << ", Rho: " << putGreeks->rho << std::endl;
+    double i_current_stock_price = UI::getDouble(">> Current Stock Price: ");
+    double i_target_price = UI::getDouble(">> Target Stock Price: ");
+    double i_target_date = UI::getDouble(">> Target date: ");
+    double i_expected_return = UI::getDouble(">> Annual Expected Return % (default 8): ");
+    i_expected_return = i_expected_return / 100;
+    double r = 0.05;    // Risk-Free Rate
+
+
+    Option atmOption(i_current_stock_price, (i_target_date / gbl::TRADING_DAYS), OptionType::Call);
+
+    double assumed_vol = 0.30;
+    FlatVolatility tempVol(assumed_vol);
+    std::optional<Greeks> suggestion = BlackScholes::calculate(atmOption.getStrike(), atmOption.getTimeToExpiry(), atmOption.getType(), i_current_stock_price, r, tempVol);
+    if(suggestion) {
+        std::cout << ">> Suggested ATM Price (assuming 30% Vol): $" << suggestion->premium << std::endl;
     }
 
-    if (callGreeks && putGreeks) {
-        checkPutCallParity(*callGreeks, *putGreeks, callOption, S, r);
+    double i_market_price = UI::getDouble(">> ATM Market Price: ");
+
+
+    std::optional<double> IV = BlackScholes::calculateIV(atmOption, i_current_stock_price, i_market_price, r);
+
+    double sigma = IV.value_or(0.30);
+    std::cout << "IV is : " << sigma << std::endl;
+    std::unique_ptr<ParametricVolatility> volModel = std::make_unique<ParametricVolatility>(sigma, -0.2, 1.0);
+    // busy day: slope:-0.5, convexity:2.5
+    // quiet day: slope:-0.05, convexity:0.3
+
+
+
+    std::vector<Strategy> strategies;
+
+    // A. Aggressive Bull: Long Call
+    strategies.push_back(Strategy::longCall(i_current_stock_price, 1.0)); // 1 Year expiry assumption
+    // B. Conservative Bull: Bull Call Spread
+    strategies.push_back(Strategy::bullCallSpread(i_current_stock_price, i_current_stock_price * 1.10, 1.0));
+    // C. Volatility Play: Straddle
+    strategies.push_back(Strategy::straddle(i_current_stock_price, 1.0));
+
+
+    std::vector<result> results;
+
+    for(const Strategy& strat : strategies) {
+        try {
+            result res = OptionWizard::simulateStrategy(strat, i_current_stock_price, i_target_price, i_target_date, r, *volModel, i_expected_return, sigma);
+            results.push_back(res);
+        } catch (const std::exception& e) {
+            std::cerr << "Error simulating " << strat.getName() << ": " << e.what() << std::endl;
+        }
     }
 
-    //    Hardcoded for now
-    //    double S = 100.0;   // Initial stock price
-    //    double r = 0.05;    // Risk-free Rate
-    //    double sigma = 0.2; // Volatility
-
-    Option call(110.0, 1.0, OptionType::Call);
-    auto temp = OptionWizard::simulateStrategy(call, 1, std::nullopt, 0, S, 110, 14, r, sigma);
-
-    std::cout << "name: " << temp.strategyName << std::endl
-                << "entryCost: " << temp.entryCost << std::endl
-                << "projectedValue: " << temp.projectedValue << std::endl
-                << "percentProfit: " << temp.profitPercent << std::endl
-                << "probability of Profit: " << temp.pop << std::endl;
+    UI::printTable(results);
 
     return 0;
 }
