7b38fadc874543538781f5b55378b8b2954fe66f
skylee01244 <skylee01244@gmail.com>
2025-12-31 06:32:35 +1100

    Added bearPutSpread n ironCondor

--- Headers/Strategy.h
+++ Headers/Strategy.h
@@ -27,4 +27,6 @@ public:
     static Strategy bullCallSpread(double K_lower, double K_higher, double T);
     static Strategy straddle(double K, double T);
     static Strategy strangle(double K_lower, double K_higher, double T);
+    static Strategy bearPutSpread(double K_higher, double K_lower, double T);
+    static Strategy ironCondor(double K_put_long, double K_put_short, double K_call_short, double K_call_long, double T);
 };
